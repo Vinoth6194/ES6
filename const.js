828b23d6dead0ea40aa9d66e7296d7ab66f8d108
Vinothkumar-mac <vinothhkumar.cse.s@gmail.com>
2020-04-28 22:14:16 +0530

    const 8

---
+++ const.js
@@ -0,0 +1,14 @@
+var a = 10;
+const  v = 100;
+//var v= 20;
+console.log(a);
+console.log(v);
+const obj = {
+    name: 'Vinoth'
+}
+console.log(obj)
+console.log(obj.name);
+obj.name = 'vinothkumar';console.log(obj.name);
+obj.add = 6;
+console.log(obj.add);
+console.log(obj);
