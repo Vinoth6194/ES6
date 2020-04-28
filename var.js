e69bd8b0c8b0e345e826462bde2c205cd087f626
Vinothkumar-mac <vinothhkumar.cse.s@gmail.com>
2020-04-28 22:05:36 +0530

    var 5

---
+++ var.js
@@ -0,0 +1,10 @@
+function greetPersons(name){
+    if(name === 'vinoth')
+    {   
+        var greet = 'Hai Vinothkumar S'
+    }else{
+        var greet = 'Hi bro'
+    }
+    console.log(greet)
+}
+greetPersons('vinoth');
