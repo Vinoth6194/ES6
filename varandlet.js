ec17c0f364dccc3ccbcf7c96376dceddb7e427ce
Vinothkumar-mac <vinothhkumar.cse.s@gmail.com>
2020-04-28 22:12:15 +0530

    var and let

---
+++ varandlet.js
@@ -0,0 +1,13 @@
+var a = 10;
+var b = 20;
+if(a === 10){
+    var a = 100;
+    let b = 200;
+    console.log(a)
+    console.log(b)
+}
+
+console.log(a)
+console.log(b);
+console.log(a)
+console.log(b)
